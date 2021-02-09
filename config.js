0f7a930de1c9a21eee737dd0a96940fe809a5621
Shivam Patel <shivampatel251097@gmail.com>
2021-02-10 01:43:02 +0530

    OAuth Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/confusion'
+    'mongoUrl': 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientid: '469457861104079',
+        clientsecret:'786418da6ae8889c4101d9743a77a6f1'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -80,4 +80,13 @@ else{
 }
 });
 
+router.get('/facebook/token',passport.authenticate('facebook-token'),(req,res)=>{
+  if(req.user){
+    var token = authenticate.getToken({id:req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json({success: true, token:token, status: 'Successfully Logged in'});
+  }
+})
+
 module.exports = router;
