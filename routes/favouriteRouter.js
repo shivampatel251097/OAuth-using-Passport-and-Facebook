a45c6a0f720690ffd65ff0e960d932f94bb58433
Shivam Patel <shivampatel251097@gmail.com>
2021-02-11 20:28:24 +0530

    assignment -4

--- routes/favouriteRouter.js
+++ routes/favouriteRouter.js
@@ -60,13 +60,14 @@ favouriteRouter.route('/')
                 user: req.user._id
             });
         }
-        //now the favourite collection has record of current user
-        // now we will push the dishes from req.body
-        console.log("current_user_favs",current_user_favs.dishes[0]._id);
-        for(let i=0;i<req.body.dishes.length;i++){
-            if((req.body.dishes[i]).indexOf(current_user_favs.dishes[i]._id)===-1){
-                current_user_favs.dishes.push(req.body.dishes[i]);
+
+        for(let i =0;i<req.body.dishes.length;i++){
+            for(let j=0;j<current_user_favs.dishes.length;j++){
+                if(req.body.dishes[i]==current_user_favs.dishes[j]._id){
+                    i=i+1;
+                }
             }
+            current_user_favs.dishes.push(req.body.dishes[i]);
         }
         current_user_favs.save()
         .then((FavDishes)=>{
@@ -79,10 +80,82 @@ favouriteRouter.route('/')
     .catch((err)=> next(err));
 })
 
-favouriteRouter.route('/test')
-.get((req,res,next)=>{
-    console.log("Hello");
-    res.send("Hello!");
+.put(authenticate.verifyUser,(req,res,next)=>{
+    res.statusCode = 403;
+    res.setHeader('Content-Type', 'application/json');
+    res.send('PUT operation not supported on /favoirites');
+    console.log('PUT operation not supported on /favoirites');
+})
+
+.delete(authenticate.verifyUser,(req,res,next)=>{
+    Favourites.find({})
+    .populate('user')
+    .populate('dishes')
+    .then((favourites)=>{
+        if(favourites){
+            current_user_favs_remove= favourites.filter(fav=> fav.user._id.toString() === req.user._id.toString())[0];
+            if(current_user_favs_remove){
+                current_user_favs_remove.remove()
+                .then((deleted)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(deleted);
+                },(err)=>next(err));
+            }
+            else{
+                var err = new Error('You have no favourites.');
+                err.status = 404;
+                return next(err);
+            }
+        }
+        else{
+            var err = new Error('You have no favourites');
+            err.status = 404;
+            return next(err);
+        }
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+
+favouriteRouter.route('/:dishID')
+.get(authenticate.verifyUser,(req,res,next)=>{
+    res.statusCode = 403;
+    res.setHeader('Content-Type', 'application/json');
+    res.send('GET operation is not supported on /favoirites/'+req.params.dishID);
+    console.log('GET operation not supported on /favoirites');
+})
+.post(authenticate.verifyUser,(req,res,next)=>{
+    Favourites.find({})
+    .populate('user')
+    .populate('dishes')
+    .then((favourites)=>{
+        var current_user_favs
+        if(favourites){
+            current_user_favs = favourites.filter(favs => favs.user._id.toString()=== req.user._id.toString())[0];
+        }
+        if(!current_user_favs){
+            current_user_favs =  new({
+                "user":req.user._id,
+            });
+        }
+        var flag
+        for(let j=0;j<current_user_favs.dishes.length;j++){
+            if(req.params.dishID==current_user_favs.dishes[j]._id){
+                flag=true;
+            }
+        }
+        if(!flag){
+            current_user_favs.dishes.push(req.params.dishID);
+        }
+        current_user_favs.save()
+        .then((Fav)=>{
+            res.statusCode=201;
+            res.setHeader("Content-Type","application/json");
+            res.json(Fav);
+        },(err)=>next(err))
+        .catch((err)=>next(err));
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
 
 module.exports = favouriteRouter;
